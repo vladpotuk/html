20cb768c13d8f9d74753ead311d81f0aaf47b82c
Влад Потук <vladpotuk01@gmail.com>
2024-07-02 18:50:46 +0300

    дфи1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await DownloadAndDisplayHamlet();
+    }
+
+    static async Task DownloadAndDisplayHamlet()
+    {
+        string url = "https://www.gutenberg.org/cache/epub/2265/pg2265.txt"; 
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode(); 
+
+                string content = await response.Content.ReadAsStringAsync(); 
+
+               
+                Console.WriteLine("Гамлет Вільяма Шекспіра:");
+                Console.WriteLine(content);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Помилка під час запиту: {e.Message}");
+            }
+        }
+    }
+}
